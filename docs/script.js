638784990cc48521194dab8ee3cdb60fd2e6dab2
tjomajDE <tjomaj526@gmail.com>
2024-09-30 10:04:24 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,22 @@
-// script.js
 document.getElementById("menu-toggle").addEventListener("click", function() {
     const nav = document.getElementById("collapsable-nav");
     nav.classList.toggle("navbar-collapse");
     const expanded = this.getAttribute("aria-expanded") === "true" || false;
     this.setAttribute("aria-expanded", !expanded);
+});
+
+// Обработчик для пунктов меню
+const menuItems = document.querySelectorAll(".menu-item");
+
+menuItems.forEach(item => {
+    item.addEventListener("click", function(event) {
+        event.preventDefault(); // Отменяем действие по умолчанию (переход по ссылке)
+
+        const targetId = this.getAttribute("data-target"); // Получаем целевой элемент
+        const targetElement = document.querySelector(targetId); // Находим элемент на странице
+
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: "smooth" }); // Прокручиваем к элементу
+        }
+    });
 });
